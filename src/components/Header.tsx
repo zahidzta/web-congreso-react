c07e12fc5c4eebabac4f76a843ec7091507406ec
zahid <zahidduron@gmail.com>
2025-03-15 23:12:42 -0600

    Modificación al header y se adapta a moviles la página principal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,16 @@ import { NavLink } from "react-router-dom"
 
 export default function Header() {
   return (
-    <header className="bg-[#090909] py-3">
+    <header className="bg-[#090909] py-3 sticky top-0 z-50">
         <div className="flex justify-between items-center">
             <NavLink to="/">
-                <div className="flex items-center gap-2.5 md:ml-5 ml-1">
+                <div className="flex items-center gap-2.5 ml-5">
                     <img className="invert w-9" src="logo.svg" alt="logo" />
-                    <span className="font-black text-white text-2xl">Congreso</span>
+                    <span className="font-black text-white text-2xl hidden md:block">Congreso</span>
                 </div>
             </NavLink>
             
-            <div className="md:mr-5 mr-1 flex gap-2">
+            <div className="mr-5 flex gap-2">
                 <NavLink to="/login">
                     <button className="md:py-1.5 py-0.5 px-2 rounded-md border-1 font-semibold cursor-pointer">Iniciar sesión</button>
                 </NavLink>
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,23 +1,28 @@
 function IndexPage() {
   return (
-    <div className="w-screen">
-        <div className="bg-[#111111] md:flex m-5 p-8 rounded-md md:justify-between">
-            <div className="">
-                <div>
-                    <h1 className="text-6xl font-black">Bienvenido <br /> al congreso <br /> <span className="bg-white text-[#111] rounded-md">2025</span></h1>
+    <div>
+        <div className="bg-[#111111] lg:flex m-3 md:m-10 p-5 md:pb-10 md:px-20 rounded-md md:justify-between items-center space-y-15">
+            <div className="space-y-10 flex flex-col items-center">
+                <div className="space-y-5">
+                    <h1 className="flex flex-wrap gap-3 text-4xl sm:text-5xl md:6xl lg:text-7xl leading-snug font-bold max-w-[400px]">
+                        <span>Bienvenido</span><span>al</span><span>congreso</span>
+                        <span className="bg-white text-[#111] rounded-md px-2">2025</span>
+                    </h1>
                     <p className="text-[#797979] font-semibold">Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Sed impedit magni <br /> est consequatur, sunt amet!</p>
                 </div>
-                <div className="space-x-4">
+                
+                <div className="space-x-5">
                     <button className="bg-white text-[#111] font-medium rounded-full py-1 px-4 cursor-pointer">Lorem</button>
                     <button className="border-white border-1 font-medium rounded-full py-1 px-4 cursor-pointer">Lorem</button>
                 </div>
+                
             </div>
-            <div className="space-y-5">
+            <div className="space-y-5 flex flex-col items-center">
                 <img 
                     className="w-lg rounded-lg"
                     src="https://cdn.prod.website-files.com/67500bfb33a314962eac96e5/67531311359326e09191a2de_hero01.avif" 
                     alt="img-congreso" />
-                <div className="flex justify-around">
+                <div className="flex justify-around flex-wrap gap-2">
                     {/*Crear un vector*/}
                     <button className="border-[#797979] border-1 py-1 px-2 rounded-full">Email Marketing</button>
                     <button className="border-[#797979] border-1 py-1 px-2 rounded-full">Social Media</button>
